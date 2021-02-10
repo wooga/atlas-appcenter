25798dc23d982cc5d5e7c0668084e270b51e1a0a
Manfred Endres <manfred.endres@tslarusso.de>
2021-02-10 09:33:26 +0100

    Implement simple retry logic in case of app extracting error
    Description
    ===========
    
    Appcenter returns an error every now and then `A problem occured while extracting your app`.
    This seems to be an internal issue as the support was not yet able
    to help out with this error. Under normal condidtion we trigger a rebuild
    and the upload works again. This little retry addition just checks for this
    specific error and retries the whole upload up to 3 times.
    
    Changes
    =======
    
    * ![IMPROVE] uploader with retry when app extraction error is thrown

---
+++ src/main/groovy/wooga/gradle/appcenter/error/AppCenterUploadException.groovy
@@ -0,0 +1,19 @@
+package wooga.gradle.appcenter.error
+
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+class AppCenterUploadException extends Exception {
+}
+
+@InheritConstructors
+class AppCenterAppExtractionException extends Exception {
+
+}
+
+@InheritConstructors
+class AppCenterMalwareDetectionException extends Exception {
+
+}
+
+
