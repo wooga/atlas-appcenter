0d1b835b515fca7148ca4ad5a080d9da7608b49a
Christian Sagel <christian.sagel@wooga.net>
2022-03-25 12:12:40 +0100

    Add commons_test library

--- src/integrationTest/groovy/wooga/gradle/appcenter/IntegrationSpec.groovy
+++ src/integrationTest/groovy/wooga/gradle/appcenter/IntegrationSpec.groovy
@@ -23,22 +23,7 @@ import org.junit.Rule
 import org.junit.contrib.java.lang.system.EnvironmentVariables
 import org.junit.contrib.java.lang.system.ProvideSystemProperty
 
-class IntegrationSpec extends nebula.test.IntegrationSpec {
-
-    @Rule
-    ProvideSystemProperty properties = new ProvideSystemProperty("ignoreDeprecations", "true")
-
-    @Rule
-    public EnvironmentVariables environmentVariables = new EnvironmentVariables()
-
-    def escapedPath(String path) {
-        String osName = System.getProperty("os.name").toLowerCase()
-        if (osName.contains("windows")) {
-            path = StringEscapeUtils.escapeJava(path)
-            return path.replace('\\', '\\\\')
-        }
-        path
-    }
+class IntegrationSpec extends com.wooga.gradle.test.IntegrationSpec {
 
     def setup() {
         def gradleVersion = System.getenv("GRADLE_VERSION")
@@ -46,63 +31,5 @@ class IntegrationSpec extends nebula.test.IntegrationSpec {
             this.gradleVersion = gradleVersion
             fork = true
         }
-    }
-
-    Boolean outputContains(ExecutionResult result, String message) {
-        result.standardOutput.contains(message) || result.standardError.contains(message)
-    }
-
-    String wrapValueBasedOnType(Object rawValue, String type) {
-        def value
-        def rawValueEscaped = String.isInstance(rawValue) ? "'${rawValue}'" : rawValue
-        def subtypeMatches = type =~ /(?<mainType>\w+)<(?<subType>[\w<>]+)>/
-        def subType = (subtypeMatches.matches()) ? subtypeMatches.group("subType") : null
-        type = (subtypeMatches.matches()) ? subtypeMatches.group("mainType") : type
-        switch (type) {
-            case "Closure":
-                if (subType) {
-                    value = "{${wrapValueBasedOnType(rawValue, subType)}}"
-                } else {
-                    value = "{$rawValueEscaped}"
-                }
-                break
-            case "Callable":
-                value = "new java.util.concurrent.Callable<${rawValue.class.typeName}>() {@Override ${rawValue.class.typeName} call() throws Exception { $rawValueEscaped }}"
-                break
-            case "Object":
-                value = "new Object() {@Override String toString() { ${rawValueEscaped}.toString() }}"
-                break
-            case "Provider":
-                switch (subType) {
-                    case "RegularFile":
-                        value = "project.layout.file(${wrapValueBasedOnType(rawValue, "Provider<File>")})"
-                        break
-                    default:
-                        value = "project.provider(${wrapValueBasedOnType(rawValue, "Closure<${subType}>")})"
-                        break
-                }
-                break
-            case "String":
-                value = "$rawValueEscaped"
-                break
-            case "String[]":
-                value = "'{${rawValue.collect { '"' + it + '"' }.join(",")}}'.split(',')"
-                break
-            case "File":
-                value = "new File('${escapedPath(rawValue.toString())}')"
-                break
-            case "String...":
-                value = "${rawValue.collect { '"' + it + '"' }.join(", ")}"
-                break
-            case "List":
-                value = "[${rawValue.collect { '"' + it + '"' }.join(", ")}]"
-                break
-            case "Long":
-                value = "${rawValue}L"
-                break
-            default:
-                value = rawValue
-        }
-        value
     }
 }
